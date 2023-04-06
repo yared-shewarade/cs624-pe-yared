78ba1764745b3c41c6500296022e6e61db3f03b4
Yared Shewarade <yared.shewarade@gmail.com>
2023-04-06 19:02:19 +0000

    first mobile app

---
+++ PE01-HelloWorld/HelloWorld/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Yared Shewarade</Text>
+      <Text>MSCS</Text>
+      <Text>STC</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffff00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
