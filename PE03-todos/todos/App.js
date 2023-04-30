2d1221d61e73c399b8c98f6571413aaaf9d278f7
yared-shewarade <yared.shewarade@gmail.com>
2023-04-30 16:50:34 -0700

    Submission for PE03

--- PE03-todos/todos/App.js
+++ PE03-todos/todos/App.js
@@ -2,15 +2,38 @@ import React, { Component } from 'react';
 import { View, ScrollView, StyleSheet } from 'react-native';
 import Input from './Input';
 import Heading from './Heading';
+import Button from './Button';
+import TodoList from './TodoList';
+
+let todoIndex = 0
 
 class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       inputValue: '',
       todos: [],
       type: 'All',
-    };
+    }
+    this.submitTodo = this.submitTodo.bind(this)
+    this.toggleComplete = this.toggleComplete.bind(this)
+    this.deleteTodo = this.deleteTodo.bind(this)
+  }
+
+  deleteTodo (todoIndex) {
+    let { todos } = this.state
+    todos = todos.filter((todo) => todo.todoIndex !== todoIndex)
+    this.setState({ todos })
+}
+
+  toggleComplete (todoIndex) {    
+    let todos = this.state.todos
+    todos.forEach((todo) => {
+      if (todo.todoIndex === todoIndex) {
+        todo.complete = !todo.complete
+      }
+    })
+    this.setState({ todos })
   }
 
   inputChange(inputValue) {
@@ -18,16 +41,22 @@ class App extends Component {
     this.setState({inputValue});
   }
 
+  submitTodo () {
+    if (this.state.inputValue.match(/^\s*$/)) {
+      return
+    }
+  }
+
   render() {
     const {inputValue} = this.state;
     return (
       <View style={styles.container}>
-        <ScrollView keyboardShouldPersistTaps="always" style={styles.content}>
+        <ScrollView 
+          keyboardShouldPersistTaps="always" 
+          style={styles.content}>
           <Heading />
-          <Input 
-            inputValue={inputValue}
-            inputChange={text => this.inputChange(text)}
-          />
+          <Input inputValue={inputValue} inputChange={text => this.inputChange(text)}/>
+          <Button submitTodo={this.submitTodo} />
         </ScrollView>
       </View>
     );
